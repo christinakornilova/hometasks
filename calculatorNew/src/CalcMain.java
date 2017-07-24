cc847cdc09a2810137cf41fe66777ae21f9e1a5c
fsage <30318622+fsage@users.noreply.github.com>
2017-07-24 23:37:35 +0300

    Round operation update.

--- calculatorNew/src/CalcMain.java
+++ calculatorNew/src/CalcMain.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class CalcMain {
@@ -17,24 +19,32 @@ public class CalcMain {
         }
     }
 
+    static double roundResult (double d) {
+
+        double newDouble = new BigDecimal(d).setScale(4, RoundingMode.HALF_UP).doubleValue();
+        return newDouble;
+
+    }
+
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
         try {
             //user input
             String s1, operation;
+            double res;
 
             do {
                 System.out.println("Input first operand (only decimal):");
                 s1 = in.next();
             } while (!isInt(s1));
-            int op1 = Integer.parseInt(s1);
+            double op1 = Integer.parseInt(s1);
 
             do {
                 System.out.println("Input second operand (only decimal):");
                 s1 = in.next();
             } while (!isInt(s1));
-            int op2 = Integer.parseInt(s1);
+            double op2 = Integer.parseInt(s1);
 
 
             System.out.println("Input operation (+, -, *, /, %):");
@@ -44,20 +54,20 @@ public class CalcMain {
             if (operation != null && (operation.equals("+") || operation.equals("-") || operation.equals("*") || operation.equals("/") || operation.equals("%"))) {
                 if (op2 == 0 && operation.equals("/")) {
                     System.out.println("Division by zero!");
-                } else if (op2 != 0 && operation.equals("/")) System.out.println("Result:" + Math.round((op1 / op2)* 10000 / 10000));
+                } else if (op2 != 0 && operation.equals("/")) {
+                    System.out.println("Result: " + roundResult(op1 / op2));
+                }
                 if (operation.equals("+")) {
-                    System.out.println("Result:" + Math.round((op1 + op2)* 10000 / 10000));
+                    System.out.println("Result: " + (op1 + op2));
                 }else if (operation.equals("-")) {
-                    System.out.println("Result:" + Math.round((op1 - op2)* 10000 / 10000));
+                    System.out.println("Result: " + (op1 - op2));
                 } else if (operation.equals("*")) {
-                    System.out.println("Result:" + Math.round((op1 * op2)* 10000 / 10000));
+                    System.out.println("Result: " + (op1 * op2));
                 } else if (op2 == 0 && operation.equals("%")) {
                     System.out.println("Division by zero!");
                 } else if (op2 != 0 && operation.equals("%"))
-                    System.out.println("Result:" + Math.round((op1 % op2)* 10000 / 10000));
+                    System.out.println("Result: " + roundResult(op1 % op2));
             } else System.out.println("Invalid entered data. Try to enter decimal values and use +, -, *, /, % for operation.");
-
-            //if (operation.equals(" ")) System.out.println("Invalid entered data. Try to enter decimal values and use +, -, *, /, % for operation.");
 
         } catch (Exception e) {
             System.err.println("Invalid entered data. Try to enter decimal values as operands and use +, -, *, /, % for operation.");
