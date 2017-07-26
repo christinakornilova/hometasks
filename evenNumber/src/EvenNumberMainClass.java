84dc6088ecdabd3bf1b76b85be74f9f09686979c
fsage <30318622+fsage@users.noreply.github.com>
2017-07-26 23:13:14 +0300

    HW#5. Parity check.

---
+++ evenNumber/src/EvenNumberMainClass.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class EvenNumberMainClass {
+
+    public static boolean isInt(String x) throws NumberFormatException
+    {
+        try {
+            Integer.parseInt(x);
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = 0;
+        String s;
+
+        //input
+        do {
+            System.out.println("Enter any int number: ");
+            s = in.next();
+        } while (!isInt(s));
+        if (isInt(s)) {
+            n = Integer.parseInt(s);
+        }
+
+        //check
+        if(n%2==0)
+            System.out.print("Number " + n + " is even");
+        else
+            System.out.print("Number " + n + " is not even");
+    }
+}
